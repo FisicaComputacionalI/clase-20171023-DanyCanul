cdd6e221aeeec206f1ca29f7f63144226f0a35e3
DanyCanul <31377538+DanyCanul@users.noreply.github.com>
2017-10-23 13:12:34 -0500

    Add files via upload
    Numeros primos

---
+++ trabajo2.cc
@@ -0,0 +1,25 @@
+//Programa que identifica numeros primos
+//Autor: Daniela Canul Garcia
+//Creado el 23 de Octubre de 2017
+#include <iostream>
+using namespace std;
+int main (){
+ int N=0;
+ int primo=0;
+ cout<<"Dame un nùmero entero "<<endl;
+ cin>>N;
+for(int i=2; i<=N/2; i++)
+{
+ if ((N%i)==0){
+ primo=1;
+ break;
+ }
+}
+if (primo==0)
+ cout<<"tu nùmero es primo"<<endl;
+else 
+ cout<<"tu numero no es primo"<<endl;
+return 0;
+}
+
+
